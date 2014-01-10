d9a5ede164ed1122eccccaa60effbfd192e3956f
Benjamin Van Ryseghem <benvr@wanadoo.fr>
2014-01-10 19:26:54 -0300

    minor fix

--- install_Triton.sh
+++ install_Triton.sh
@@ -1,6 +1,3 @@
-curl -o script.st -L https://raw.github.com/BenjaminVanRyseghem/Triton/master/installScript.st
-
-
 if [ "$#" -ne 1 ]; then
     echo "Usage:
 ./install_Triton Pharo.image" 1>&2
@@ -9,6 +6,7 @@ fi
 
 rm -rf pharo-vm
 
+curl -o script.st -L https://raw.github.com/BenjaminVanRyseghem/Triton/master/installScript.st
 curl -o spec.zip -L https://github.com/SpecForPharo/spec/archive/master.zip
 curl -o triton.zip -L https://github.com/BenjaminVanRyseghem/Triton/archive/master.zip
 unzip -u spec.zip
