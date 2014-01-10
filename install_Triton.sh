a6bc8d15e06d8ed3003d74fa9f2db12125e1b392
Benjamin Van Ryseghem <benvr@wanadoo.fr>
2014-01-10 18:13:14 -0300

    now the script use a script from github, allowing updates

--- install_Triton.sh
+++ install_Triton.sh
@@ -1,3 +1,6 @@
+curl -o script.st -L https://raw.github.com/BenjaminVanRyseghem/Triton/master/installScript.st
+
+
 if [ "$#" -ne 1 ]; then
     echo "Usage:
 ./install_Triton Pharo.image" 1>&2
@@ -12,51 +15,6 @@ unzip -u spec.zip
 unzip -u triton.zip
 
 IMAGE=$PWD/$1
+SCRIPT=`cat script.st`
 
-curl get.pharo.org/vm | bash
-
-./pharo "$IMAGE" eval --save "
-| repo names currentVersions toLoad |
-
-[ [ Gofer it
-	smalltalkhubUser: 'ObjectProfile' project: 'Roassal2';
-	package: 'Roassal2';
-	package: 'Trachel';
-	package: 'Roassal2Spec';
-	load ]
-	on: MCMergeOrLoadWarning
-	do: [ :ex | ex merge ] ] 
-	on: MCNoChangesException 
-	do: [] .
-
-repo := MCFileTreeRepository new directory: '$PWD/spec-master' asFileReference.
-
-currentVersions := MCWorkingCopy allManagers select: [ :e | (e package name beginsWith: 'SLICE') not ] thenCollect: [ :e | e ancestry ancestors first name ].
-names := repo packageDescriptionsFromReadableFileNames.
-toLoad := names select: [ :e || currentVersion | 
-	currentVersion := e first , '-', e second , '.', e third asString.
-	(currentVersions includes: currentVersion) not ].
-
-toLoad do: [ :name || version |
-	version := repo versionFromFileNamed: name first, '.package'.
-	FileStream stdout << name first << '-'<< name second << '.'<< name third asString; crlf.
-	[ version load ]
-		on: MCMergeOrLoadWarning
-		do: [ :ex | ex load ] ].
-
-repo := MCFileTreeRepository new directory: '$PWD/Triton-master' asFileReference.
-
-currentVersions := MCWorkingCopy allManagers select: [ :e | (e package name beginsWith: 'SLICE' ) not ] thenCollect: [ :e | e ancestry ancestors first name ].
-names := repo packageDescriptionsFromReadableFileNames.
-toLoad := names select: [ :e || currentVersion | 
-	currentVersion := e first , '-', e second , '.', e third asString.
-	(currentVersions includes: currentVersion) not ].
-
-toLoad do: [ :name || version |
-	version := repo versionFromFileNamed: name first, '.package'.
-	FileStream stdout << name first << '-'<< name second << '.'<< name third asString; crlf.
-	[ version load ]
-		on: MCMergeOrLoadWarning
-		do: [ :ex | [ ex load  ] on: MCNoChangesException do: [] ] ].
-
-"
+./pharo "$IMAGE" eval --save "$SCRIPT"
